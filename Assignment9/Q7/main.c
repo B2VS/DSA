920da085b8cc68c8ce2d3687f3c43b532d4eea76
Vedic Sharma <mevs2000@gmail.com>
2017-04-10 15:37:20 +0530

    A9 Q7 updated

--- Assignment9/Q7/main.c
+++ Assignment9/Q7/main.c
@@ -50,35 +50,6 @@ void MaxHeap(int *a, int *b, int *c, int size)
         MaxHeapify(a, b, c, i, size);
 }
 
-void Insert(int *a, int *b, int *c, int *size, int data, int row, int col)
-{
-    ++*size;
-    a[*size] = data;
-    b[*size] = row;
-    c[*size] = col;
-    int i = *size;
-    while (i > 1)
-    {
-        if (a[Parent(i)] < a[i])
-        {
-            swap(a + Parent(i), a + i);
-            swap(b + Parent(i), b + i);
-            swap(c + Parent(i), c + i);
-        }
-        i = Parent(i);
-    }
-}
-
-void DeleteMax(int *a, int *b, int *c, int *size)
-{
-    a[1] = a[*size];
-    b[1] = b[*size];
-    c[1] = c[*size];
-    *size -= 1;
-    if (*size == 0) return;
-    MaxHeapify(a, b, c, 1, *size);
-}
-
 int main()
 {
     int n, m, i, j, count = 0, size;
@@ -100,16 +71,25 @@ int main()
         col[i] = 0;
     }
     MaxHeap(heap - 1, row - 1, col - 1, n);
-    size = n;
     while(count < n)
     {
         printf("%d %d %d\n", heap[0], row[0], col[0]);
         int tr = row[0];
         int tc = col[0];
-        DeleteMax(heap - 1, row - 1, col - 1, &size);
         if (tc + 1 < m)
-            Insert(heap - 1, row - 1, col - 1, &size, arr[tr][tc + 1], tr, tc + 1);
+        {
+            heap[0] = arr[tr][tc + 1];
+            row[0] = tr;
+            col[0] = tc + 1;
+            MaxHeap(heap - 1, row - 1, col - 1, n);
+        }
         else
+        {
+            heap[0] = 0;
+            row[0] = 0;
+            col[0] = 0;
+            MaxHeap(heap - 1, row - 1, col - 1, n);
             ++count;
+        }
     }
 }
